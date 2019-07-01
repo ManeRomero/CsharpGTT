a6c6539109fd94b13874dccb3aad5e7ac3ebe8ca
formacion <formacion@gtt.es>
2019-07-01 14:57:14 +0200

    home-to-do job

---
+++ C#/05projectNumerosRomanos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05projectNumerosRomanos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = calculaNums(Convert.ToInt16(args[0]));
+            Console.Write(result);
+        }
+
+        static string calculaNums(int year)
+        {
+            if (year >= 1000)
+            {
+                int millares = year / 1000;
+                return new String('M', millares);
+            }
+            else if (year < 1000 && year >= 500)
+            {
+                int quinientares = year / 500;
+                return new String('D', quinientares);
+            }
+        }
+    }
+}
