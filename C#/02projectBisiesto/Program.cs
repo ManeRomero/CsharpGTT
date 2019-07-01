42082e12df6c4af6a39bf33af677b5157c8aaeee
formacion <formacion@gtt.es>
2019-07-01 12:13:28 +0200

    Program 'Bisiesto'

---
+++ C#/02projectBisiesto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02projectBisiesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = Convert.ToInt32(args[0]);
+            bool result = isBisiesto(year);
+            Console.WriteLine(result);
+        }
+
+        static bool isBisiesto(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+        }
+    }
+}
