228a6a2d221165bdb894ac854890dc3f45aaa88c
formacion <formacion@gtt.es>
2019-07-05 10:14:43 +0200

    api exercise Store developed

--- ApiExercises/TodoApi/Controllers/TodoController.cs
+++ ApiExercises/TodoApi/Controllers/TodoController.cs
@@ -4,26 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TodoApi.Models;
+using TodoApi.utils;
 
 namespace TodoApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class TodoController : ControllerBase
-    {
-        private List<TodoItem> todos = new List<TodoItem>();
+    { 
+        public TodoController() {
+            Console.Write("CONSOLE DE TODA LA VIDA \n\n\n\n\n\nFIN####");
+        }
+        private Store store = Store.Instance;
 
         [HttpGet]
-        public List <TodoItem> GetTodoItems()
+        public Store GetTodoItems()
         {
-            return this.todos;
+            return this.store;
         }
 
         [HttpPost]
-
         public TodoItem PostTodo(TodoItem item)
         {
-            this.todos.Add(item);
+            this.store.todos.Add(item);
             return item;
         }
     }
---
+++ ApiExercises/TodoApi/utils/Store.cs
@@ -0,0 +1,48 @@
+using TodoApi.Models;
+using System.Collections.Generic;
+
+namespace TodoApi.utils
+{
+    public sealed class Store
+    {
+        private readonly static Store _instance = new Store();
+        public List<TodoItem> todos = new List<TodoItem>();
+        public Store()
+        {
+
+        }
+
+        public static Store Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
+
+
+/* public sealed class MyClass
+{
+    private static volatile IMyObject instance;
+    private static Object syncRootObject = new Object();
+
+    public static IMyObject Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (syncRooObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new MyObject();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+} */
